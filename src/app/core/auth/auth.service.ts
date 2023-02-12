e9bb06fa8752ff5807635155245cbafff0d11ea9
doosterhere <doosterhere@gmail.com>
2023-02-12 23:05:48 +0300

    login and signup have been completed

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,93 @@
+import {Injectable} from '@angular/core';
+import {Observable, Subject, throwError} from "rxjs";
+import {DefaultResponseType} from "../../../types/default-response.type";
+import {LoginResponseType} from "../../../types/login-response.type";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public accessTokenKey: string = 'accessToken';
+  public refreshTokenKey: string = 'refreshToken';
+  public userIdKey: string = 'userId';
+  public isLogged$: Subject<boolean> = new Subject<boolean>();
+  private _isLogged: boolean = false;
+  private _userId: string | null = null;
+
+  constructor(private httpClient: HttpClient) {
+    this.isLogged = !!localStorage.getItem(this.accessTokenKey);
+  }
+
+  get isLogged() {
+    return this._isLogged;
+  }
+
+  set isLogged(value: boolean) {
+    this._isLogged = value;
+  }
+
+  get userId(): string | null {
+    return localStorage.getItem(this.userIdKey);
+  }
+
+  set userId(userId: string | null) {
+    if (userId) {
+      localStorage.setItem(this.userIdKey, userId);
+      return;
+    }
+
+    if (!userId) {
+      localStorage.removeItem(this.userIdKey);
+    }
+  }
+
+  login(email: string, password: string, rememberMe: boolean): Observable<DefaultResponseType | LoginResponseType> {
+    return this.httpClient.post<DefaultResponseType | LoginResponseType>(environment.api + 'login', {
+      email,
+      password,
+      rememberMe
+    });
+  }
+
+  signup(email: string, password: string, passwordRepeat: string): Observable<DefaultResponseType | LoginResponseType> {
+    return this.httpClient.post<DefaultResponseType | LoginResponseType>(environment.api + 'signup', {
+      email,
+      password,
+      passwordRepeat
+    });
+  }
+
+  logout(): Observable<DefaultResponseType> {
+    const tokens = this.getTokens();
+    if (tokens && tokens.refreshToken) {
+      return this.httpClient.post<DefaultResponseType>(environment.api + 'logout', {
+        refreshToken: tokens.refreshToken
+      });
+    }
+
+    throw throwError(() => 'Can\'t find refresh token');
+  }
+
+  setTokens(accessToken: string, refreshToken: string): void {
+    localStorage.setItem(this.accessTokenKey, accessToken);
+    localStorage.setItem(this.refreshTokenKey, refreshToken);
+    this.isLogged = true;
+    this.isLogged$.next(this.isLogged);
+  }
+
+  removeTokens(): void {
+    localStorage.removeItem(this.accessTokenKey);
+    localStorage.removeItem(this.refreshTokenKey);
+    this.isLogged = false;
+    this.isLogged$.next(this.isLogged);
+  }
+
+  getTokens(): { accessToken: string | null, refreshToken: string | null } {
+    return {
+      accessToken: localStorage.getItem(this.accessTokenKey),
+      refreshToken: localStorage.getItem(this.refreshTokenKey)
+    }
+  }
+}
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {CategoryType} from "../../../../types/category.type";
+import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {HttpErrorResponse} from "@angular/common/http";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -8,11 +13,36 @@ import {CategoryType} from "../../../../types/category.type";
 })
 export class HeaderComponent implements OnInit {
   @Input() categories: CategoryType[] = [];
+  isLogged: boolean = false;
 
-  constructor() {
+  constructor(private authService: AuthService,
+              private _snackBar: MatSnackBar,
+              private router: Router) {
+    this.isLogged = this.authService.isLogged;
   }
 
   ngOnInit(): void {
+    this.authService.isLogged$.subscribe((isLogged: boolean) => {
+      this.isLogged = isLogged;
+    });
   }
 
+  logout(): void {
+    this.authService.logout()
+      .subscribe({
+        next: () => {
+          this.doLogout();
+        },
+        error: () => {
+          this.doLogout();
+        }
+      });
+  }
+
+  doLogout(): void {
+    this.authService.removeTokens();
+    this.authService.userId = null;
+    this._snackBar.open('Вы вышли из системы');
+    this.router.navigate(['/']);
+  }
 }

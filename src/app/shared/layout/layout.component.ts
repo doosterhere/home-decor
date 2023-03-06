7ea0718d654ad9ffd4350f17f408dcb753d9b5a8
doosterhere <doosterhere@gmail.com>
2023-03-06 12:26:57 +0300

    some minor changes in _global.scss have been made

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,19 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CategoryService} from "../services/category.service";
 import {CategoryWithTypesType} from "../../../types/category-with-types.type";
+import {Subscription} from "rxjs";
 
 @Component({
-  selector: 'layout',
+  selector: 'app-layout',
   templateUrl: './layout.component.html'
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit, OnDestroy {
   categories: CategoryWithTypesType[] = [];
+  categoryServiceGetCategoriesWithTypesSubscription: Subscription | null = null;
 
   constructor(private categoryService: CategoryService) {
   }
 
   ngOnInit(): void {
-    this.categoryService.getCategoriesWithTypes()
+    this.categoryServiceGetCategoriesWithTypesSubscription = this.categoryService.getCategoriesWithTypes()
       .subscribe({
         next: (categories: CategoryWithTypesType[]) => {
           if (categories) {
@@ -26,6 +28,10 @@ export class LayoutComponent implements OnInit {
           throw new Error(error.message);
         }
       })
+  }
+
+  ngOnDestroy(): void {
+    this.categoryServiceGetCategoriesWithTypesSubscription?.unsubscribe();
   }
 
 }

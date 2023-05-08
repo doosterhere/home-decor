2fb7057afe8ede4bf8572d92330583d29f1c272e
Mikalai Shcharbakou <doosterhere@gmail.com>
2023-05-08 13:41:56 +0300

    the next changes have been made:
    1. environment.prod.ts has been changed for the remote backend;
    2. A new script (built-gp) has been added in the package.json;
    3. Hints for inputs in Login and Signup components have been added;
    4. Yandex map in the Main component has been replaced with the Google
       one;
    5. Social links in Footer component now aren't dummies;
    6. Links in Header and Footer components now change color when hover;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api: "http://localhost:3000/api/",
-  serverStaticPath: "http://localhost:3000/images/products/"
+  api: "https://home-decor-backend.vercel.app/api/",
+  serverStaticPath: "https://home-decor-backend.vercel.app/images/products/"
 }
